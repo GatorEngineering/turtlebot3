dcfdca561f608461872d740fd00b63d7139c9065
acalvo <acalvo1@ufl.edu>
2025-03-13 22:12:18 -0400

    Trying deleteing unnecessary files again

--- install/turtlebot3/share/turtlebot3/cmake/turtlebot3Config-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/acalvo/turtlebot3_ws/build/turtlebot3/ament_cmake_core/turtlebot3Config-version.cmake
--- install/turtlebot3/share/turtlebot3/local_setup.bash
+++
@@ -1,1 +0,0 @@
-/home/acalvo/turtlebot3_ws/build/turtlebot3/ament_cmake_environment_hooks/local_setup.bash
